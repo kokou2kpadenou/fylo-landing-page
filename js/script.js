23f054a6f71d873716e391e401fcbccc3a269038
kokou kpadenou <kkokou@gmail.com>
2019-03-23 16:12:32 -0400

    build-2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 onload = start;
 
-const themeLight = document.getElementById('theme-light');
-const theme = document.getElementsByClassName('theme__radio');
-const popup = document.getElementsByClassName('popup');
+var themeLight = document.getElementById('theme-light');
+var theme = document.getElementsByClassName('theme__radio');
+var popup = document.getElementsByClassName('popup');
 
 function start() {
   // Read the local storage for the theme set in the pass
   if ('localStorage' in window) {
-    let local = localStorage.getItem('fyloTheme');
+    var local = localStorage.getItem('fyloTheme');
     if (local) {
       defaultTheme = local;
     } else {
@@ -28,13 +28,13 @@ function closePopup(e) {
   }
 }
 
-for (let j=0; j < popup.length; j++) {
+for (var j=0; j < popup.length; j++) {
 	popup[j].addEventListener('click', closePopup);
 }
 
 
 // Update the local storage with the new theme for future use
-for (let j=0; j < theme.length; j++) {
+for (var j=0; j < theme.length; j++) {
 	theme[j].addEventListener('click', switchTheme);
 }
 
